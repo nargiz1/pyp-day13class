8792458c9749d9c9ed3b02ed97449e5ce75600ea
Nargiz Ramazanova <nargiz.rp@code.local>
2022-09-08 10:25:11 +0400

    frst

--- main.js
+++ main.js
@@ -1,20 +1,29 @@
 let passwordInput = document.querySelector("#password");
+let overlay = document.querySelector("img");
 
+let initialBlur = 10;
 
 passwordInput.addEventListener("input", function () {
   if (passwordInput.value) {
     if (eightCharacters(passwordInput.value)) {
-      
+      initialBlur = initialBlur - 2;
+      overlay.style.filter = `blur(${initialBlur - 2}px)`;
     }
-    if (specialCharacter(passwordInput.value)) {
+    if (specialCharacter(passwordInput.value[passwordInput.value.length-1])) {
       console.log("special character");
+      initialBlur = initialBlur - 2;
+      overlay.style.filter = `blur(${initialBlur - 2}px)`;
     }
-    if (uppercaseLetter(passwordInput.value)) {
+    if (uppercaseLetter(passwordInput.value[passwordInput.value.length-1])) {
       console.log("UppercaseLetter");
+      initialBlur = initialBlur - 2;
+      overlay.style.filter = `blur(${initialBlur - 2}px)`;
     }
-    if (number(passwordInput.value)) {
-        console.log("number");
-      }
+    if (number(passwordInput.value[passwordInput.value.length-1])) {
+      console.log("number");
+      initialBlur = initialBlur - 2;
+      overlay.style.filter = `blur(${initialBlur - 2}px)`;
+    }
   }
 });
 
@@ -25,25 +34,19 @@ function eightCharacters(val) {
 }
 
 function specialCharacter(val) {
-  for (var i = 0; i < val.length; i++) {
-    if (val.charCodeAt(i) >= 33 && val.charCodeAt(i) <= 38) {
+    if (val.charCodeAt(0) >= 33 && val.charCodeAt(0) <= 38) {
       return true;
     }
-  }
 }
 
 function uppercaseLetter(val) {
-  for (var i = 0; i < val.length; i++) {
-    if (val.charCodeAt(i) >= 65 && val.charCodeAt(i) <= 90) {
+    if (val.charCodeAt(0) >= 65 && val.charCodeAt(0) <= 90) {
       return true;
     }
-  }
 }
 
 function number(val) {
-    for (var i = 0; i < val.length; i++) {
-      if (Number(val[i])) {
-        return true;
-      }
+    if (Number(val)) {
+      return true;
     }
-  }
+}
